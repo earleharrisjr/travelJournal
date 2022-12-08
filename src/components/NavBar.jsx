1fdeedb835b8e3d36afa4bfb7dab06e4a5aa9c42
Earl Harris <earleharrisjr@gmail.com>
2022-12-08 10:13:05 -0500

    styled icon for nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaGlobeAmericas } from 'react-icons/fa';
 
 export default function NavBar() {
 
     return (
-        <nav>Earl's Travels</nav>
+        <nav><FaGlobeAmericas className="iconGlobe"/> Earl's Travels</nav>
     )
 }
